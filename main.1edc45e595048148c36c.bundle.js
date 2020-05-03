642ee70f7c6ece693f2c900bd0a2fca8d684518c
Gauca <gaucab@gb-locs-MacBook-Pro.local>
2020-05-03 21:26:07 +0300

    Updates

---
+++ main.1edc45e595048148c36c.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{470:function(n,o,p){p(471),p(614),p(1262),p(1296),n.exports=p(1299)},533:function(n,o){}},[[470,1,2]]]);
---
+++ precache-manifest.b02076aab59ae1b115286387652ae18e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40098d89ea437dee12de61064a5368e9",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c2acc98946b0c7a46c48.bundle.js"
+  },
+  {
+    "url": "runtime~main.c2acc98946b0c7a46c48.bundle.js"
+  },
+  {
+    "revision": "a65a3f20f59dba30f6d7",
+    "url": "static/css/main.bf5e5510.chunk.css"
+  },
+  {
+    "url": "vendors~main.c2acc98946b0c7a46c48.bundle.js"
+  },
+  {
+    "url": "vendors~main.c2acc98946b0c7a46c48.bundle.js.LICENSE.txt"
+  }
+]);
